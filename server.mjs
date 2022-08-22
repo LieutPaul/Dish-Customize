7b07bcc53508404734360967772a978cdb5a0481
Vikas Kalyanapuram <vikaskaly@Vikass-MacBook-Air.local>
2022-08-22 11:20:11 +0530

    server commit

--- server.mjs
+++ server.mjs
@@ -2,10 +2,10 @@ import fetch from 'node-fetch';
 import express from 'express';
 import bodyParser from "body-parser";
 
-//const express = require('express');
-//const https=require('https');
-//const bodyParser = require('body-parser');
-//const { get } = require('http');
+// const express = require('express');
+// const https=require('https');
+// const bodyParser = require('body-parser');
+// const { get } = require('http');
 
 const app = express();
 
